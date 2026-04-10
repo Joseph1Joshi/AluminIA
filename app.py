cdf0038788b5cedb6ff2c21ba67b3f8bf1c86bbb
Joseph Joshi <torifiojosephedc@gmail.com>
2026-04-10 13:14:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.components.v1.html(
     <script>
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', function() {
-        navigator.serviceWorker.register('https://tu-app.streamlit.app/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('https://aluminia.streamlit.app/sw.js').then(function(registration) {
           console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
           console.log('ServiceWorker registration failed: ', err);
